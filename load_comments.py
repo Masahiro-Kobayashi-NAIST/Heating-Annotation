f565ed4f413aa6fcb83502c2d95e77ae42051e2d
MKobayashi <kobayashi.masahiro.kl0@g.ext.naist.jp>
2022-10-18 01:51:56 +0900

    fix a typo

--- load_comments.py
+++ load_comments.py
@@ -6,7 +6,7 @@ def load_html(n):
     with open(PATH, "r") as f:
         d = json.load(f)
 
-    cmts = d["commnts"]
+    cmts = d["comments"]
     cmt_lvs = d["comment_levels"]
     pid = d["pageid"]
     title = d["title"].lstrip("Wikipedia:井戸端/subj/")
